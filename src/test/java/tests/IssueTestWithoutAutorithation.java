bf70a162f389f2fc4678811faf32648ac5fcb70c
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-05-07 21:12:43 +0300

    new

--- src/test/java/tests/IssueTestWithoutAutorithation.java
+++ src/test/java/tests/IssueTestWithoutAutorithation.java
@@ -1,8 +1,10 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import steps.GitHubSteps;
 
+import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class IssueTestWithoutAutorithation {
@@ -16,9 +18,10 @@ public class IssueTestWithoutAutorithation {
     }
     @Test
     public void sportMast(){
-        open("https://www.google.com/?hl=ru");
-        $("#APjFqb").setValue("Спортмастер").pressEnter();
-        $x("//h3[contains(text(),'Спортмастер — спортивный магазин для всей семьи!')]").click();
-        sleep(5000);
+        open("https://bi.zone/");
+        $x("//span[contains(text(),'Экспертиза')]").hover();
+        $(byTagAndText("a", "Материалы")).click();
+        $x("//h1[contains(text(),'Материалы')]").shouldBe(Condition.visible);
+
     }
 }
